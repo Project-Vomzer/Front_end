a0c53d14cd5573b0245080a2bf18455ae842c017
davidojo1144 <davco1144@gmail.com>
2025-05-11 20:14:19 +0100

    Vomzer Sui

--- Vomzer_sui_socials/src/components/Navbar.jsx
+++ Vomzer_sui_socials/src/components/Navbar.jsx
@@ -4,16 +4,16 @@ import { assets } from '../assets/assets'
 const Navbar = () => {
   return (
     <div>
-        <div className='md:flex items-center justify-evenly hidden'>
+        <div className='md:flex items-center justify-evenly hidden pt-10'>
             <img src={assets.Vomzer} alt="" />
-            <input type="text" placeholder='Search....'/>
+            <input className='rounded-2xl px-10 py-1 text-black outline-none focus:border-blue-400 hover:' type="text" placeholder='Search....'/>
             <img src={assets.home} alt="" />
-            <img src="" alt="" />
-            <img src="" alt="" />
-            <img src="" alt="" />
-            <img src="" alt="" />
-            <img src="" alt="" />
-            <img src="" alt="" />
+            <img src={assets.feeds} alt="" />
+            <img src={assets.notification} alt="" />
+            <img src={assets.chatmessages} alt="" />
+            <img src={assets.wallet} alt="" />
+            <img src={assets.services} alt="" />
+            <img src={assets.Dots} alt="" />
             <img src="" alt="" />
         </div>
     </div>
