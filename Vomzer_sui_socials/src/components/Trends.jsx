eb3a59d0ac0f5a336a4c8850376f63a70f00c705
davidojo1144 <davco1144@gmail.com>
2025-05-11 23:05:06 +0100

    Vomzer Sui

--- Vomzer_sui_socials/src/components/Trends.jsx
+++ Vomzer_sui_socials/src/components/Trends.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Trends = () => {
   return (
     <div>
-      
+      <div>
+        
+      </div>
     </div>
   )
 }
--- Vomzer_sui_socials/src/pages/Home.jsx
+++ Vomzer_sui_socials/src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Trends from '../components/Trends'
+import Feeds from '../components/Feeds'
+import Sui from '../components/Sui'
 
 const Home = () => {
   return (
     <div>
-      
+      <Trends/>
+      <Feeds/>
+      <Sui/>
     </div>
   )
 }
