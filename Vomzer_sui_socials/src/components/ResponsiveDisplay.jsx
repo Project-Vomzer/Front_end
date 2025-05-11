e97147f6fff1ba9f5eea5f26f6629dedfa434171
davidojo1144 <davco1144@gmail.com>
2025-05-12 00:28:16 +0100

    Vomzer Sui

--- Vomzer_sui_socials/src/components/ResponsiveDisplay.jsx
+++ Vomzer_sui_socials/src/components/ResponsiveDisplay.jsx
@@ -17,31 +17,31 @@ const ResponsiveDisplay = ({img}) => {
                 <div className='bg-gradient-to-r from-blue-400 to-teal-400  text-white text-sm font-semibold uppercase py-10 mt-10 rounded-3xl md:hidden'>
                     <ul className='flex flex-col items-center justify-center  gap-7'>
                         <div className='flex gap-5'>
-                            <Link to="/" ><li>Home</li></Link>
+                            {/* <Link to="/" ><li>Home</li></Link> */}
                             <img className='w-5' src={assets.home} alt="" />
                         </div>
                         <div className='flex gap-5'>
-                            <Link to="/" ><li>feeds</li></Link>
+                            {/* <Link to="/" ><li>feeds</li></Link> */}
                             <img className='w-5' src={assets.feeds} alt="" />
                         </div>
                         <div className='flex gap-2'>
-                            <Link to="/" ><li>notification</li></Link>
+                            {/* <Link to="/" ><li>notification</li></Link> */}
                             <img className='w-5' src={assets.notification} alt="" />
                         </div>
                         <div className='flex gap-5'>
-                            <Link to="/" ><li>messages</li></Link>
+                            {/* <Link to="/" ><li>messages</li></Link> */}
                             <img className='w-5' src={assets.chatmessages} alt="" />
                         </div>
                         <div className='flex gap-5'>
-                            <Link to="/" ><li>wallet</li></Link>
+                            {/* <Link to="/" ><li>wallet</li></Link> */}
                             <img className='w-5' src={assets.wallet} alt="" />
                         </div>
                         <div className='flex gap-5'>
-                            <Link to="/" ><li>services</li></Link>
+                            {/* <Link to="/" ><li>services</li></Link> */}
                             <img className='w-5' src={assets.services} alt="" />
                         </div>
                         <div className='flex gap-5'>
-                            <Link to="/" ><li>menu</li></Link>
+                            {/* <Link to="/" ><li>menu</li></Link> */}
                             <img className='w-5' src={assets.Dots} alt="" />
                         </div>
                     </ul>
--- Vomzer_sui_socials/src/components/Trends.jsx
+++ Vomzer_sui_socials/src/components/Trends.jsx
@@ -7,6 +7,7 @@ const Trends = () => {
       <div>
         <img className='w-16 pt-10 ml-[40%]' src={assets.logo} alt="" />
         <h1 className='text-xl font-semibold text-center mt-5'>Vomzer</h1>
+        <p className='text-md text-center mt-2'>@Vomzer Socials</p>
         <div>
 
         </div>
