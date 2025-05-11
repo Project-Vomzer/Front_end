c9d78af4b25f6b2fb4e496961cc78a8c54bf4b04
davidojo1144 <davco1144@gmail.com>
2025-05-11 21:31:48 +0100

    Vomzer Sui

--- Vomzer_sui_socials/src/components/ResponsiveDisplay.jsx
+++ Vomzer_sui_socials/src/components/ResponsiveDisplay.jsx
@@ -12,11 +12,13 @@ const ResponsiveDisplay = ({img}) => {
             animate={{ opacity:1, y: 0 }}
             exit={{ opacity:0, y: -100 }}
             transition={{duration: 0.3}}
-            className=' top-[8] left-0 w-full z-10'
             >
-                <div className='bg-teal-500 text-white text-sm font-semibold uppercase py-10 m-6 rounded-3xl md:hidden'>
+                <div className='bg-gradient-to-r from-blue-400 to-teal-400 text-white text-sm font-semibold uppercase py-10 m-6 rounded-3xl md:hidden'>
                     <ul className='flex flex-col items-center justify-center  gap-7'>
-                        <Link to="/" ><li>Home</li></Link>
+                        <div className='flex'>
+                            <Link to="/" ><li>Home</li></Link>
+                            <img src="" alt="" />
+                        </div>
                         <Link to="/buy" ><li>Buy</li></Link>
                         <Link to="/sell" ><li>Sell</li></Link>
                         <Link to="/about" ><li>About</li></Link>
