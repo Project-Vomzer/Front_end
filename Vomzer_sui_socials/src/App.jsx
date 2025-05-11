a2ed685f41f00ef9898defa033f1013f24953328
davidojo1144 <davco1144@gmail.com>
2025-05-11 19:50:14 +0100

    Vomzer Sui

---
+++ Vomzer_sui_socials/src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { AnimatePresence, motion } from 'framer-motion';
+import { Routes, Route, useLocation } from 'react-router-dom'
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+
+
+
+const App = () => {
+  const location = useLocation()
+
+  return (
+    <AnimatePresence mode='wait'>
+    <div className='overflow-x-hidden app-background'>
+      <Navbar/>
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={ <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.5 }}> <Home/> </motion.div> } />
+        </Routes>
+        <ToastContainer/>
+      <Footer/>
+    </div>
+    </AnimatePresence>
+  )
+}
+
+export default App
---
+++ Vomzer_sui_socials/src/assets/assets.js
@@ -0,0 +1,23 @@
+import background from "./background.png"
+import chatmessages from "./chatmessages.png"
+import Dots from "./Dots.png"
+import feeds from "./feeds.png"
+import home from "./Home.png"
+import notification from "./Notification.png"
+import services from "./Services.png"
+import wallet from "./Wallet.png"
+
+
+
+
+
+export const assets = {
+    background,
+    chatmessages,
+    Dots,
+    feeds,
+    home,
+    notification,
+    services,
+    wallet
+}
