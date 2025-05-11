0703e371664a8d1b1e458288fb9559ab84c46231
davidojo1144 <davco1144@gmail.com>
2025-05-12 00:07:53 +0100

    Vomzer Sui

--- Vomzer_sui_socials/src/components/Trends.jsx
+++ Vomzer_sui_socials/src/components/Trends.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Trends = () => {
   return (
-    <div className='bg-white h-10 rounded w-full md:w-[30%]'>
-      <div >
-        
+    <div className='bg-white  rounded-2xl w-full md:w-[30%]'>
+      <div>
+        <img className='w-20 items-center justify-center pt-20' src={assets.logo} alt="" />
+        <div>
+
+        </div>
       </div>
     </div>
   )
