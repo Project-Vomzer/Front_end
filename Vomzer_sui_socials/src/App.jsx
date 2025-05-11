05ee77f715c366dae95c7eb72e6e4e056aac4918
davidojo1144 <davco1144@gmail.com>
2025-05-11 20:01:20 +0100

    Vomzer Sui

--- Vomzer_sui_socials/src/App.jsx
+++ Vomzer_sui_socials/src/App.jsx
@@ -3,9 +3,9 @@ import { AnimatePresence, motion } from 'framer-motion';
 import { Routes, Route, useLocation } from 'react-router-dom'
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
 import Home from './pages/Home'
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 
 
@@ -14,7 +14,7 @@ const App = () => {
 
   return (
     <AnimatePresence mode='wait'>
-    <div className='overflow-x-hidden app-background'>
+    <div className='app-background overflow-x-hidden'>
       <Navbar/>
         <Routes location={location} key={location.pathname}>
           <Route path="/" element={ <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.5 }}> <Home/> </motion.div> } />
---
+++ Vomzer_sui_socials/src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>
+        <div>
+            <div>
+            <img src="" alt="" />
+            <img src="" alt="" />
+            <img src="" alt="" />
+            <img src="" alt="" />
+            <img src="" alt="" />
+            <img src="" alt="" />
+            <img src="" alt="" />
+            <img src="" alt="" />
+            <img src="" alt="" />
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
