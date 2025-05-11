bc1a581adad69799c4d80cb886d1b5abe262de82
davidojo1144 <davco1144@gmail.com>
2025-05-11 21:35:16 +0100

    Vomzer Sui

--- Vomzer_sui_socials/src/components/ResponsiveDisplay.jsx
+++ Vomzer_sui_socials/src/components/ResponsiveDisplay.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { AnimatePresence,motion } from 'framer-motion'
 import { Link } from 'react-router-dom'
+import { assets } from '../assets/assets'
 
 const ResponsiveDisplay = ({img}) => {
 
@@ -15,14 +16,34 @@ const ResponsiveDisplay = ({img}) => {
             >
                 <div className='bg-gradient-to-r from-blue-400 to-teal-400 text-white text-sm font-semibold uppercase py-10 m-6 rounded-3xl md:hidden'>
                     <ul className='flex flex-col items-center justify-center  gap-7'>
-                        <div className='flex'>
+                        <div className='flex gap-5'>
                             <Link to="/" ><li>Home</li></Link>
-                            <img src="" alt="" />
+                            <img className='w-5' src={assets.home} alt="" />
                         </div>
-                        <Link to="/buy" ><li>Buy</li></Link>
-                        <Link to="/sell" ><li>Sell</li></Link>
-                        <Link to="/about" ><li>About</li></Link>
-                        <Link to="/contactus" ><li>Contact Us</li></Link>
+                        <div className='flex gap-5'>
+                            <Link to="/" ><li>Home</li></Link>
+                            <img className='w-5' src={assets.home} alt="" />
+                        </div>
+                        <div className='flex gap-5'>
+                            <Link to="/" ><li>Home</li></Link>
+                            <img className='w-5' src={assets.home} alt="" />
+                        </div>
+                        <div className='flex gap-5'>
+                            <Link to="/" ><li>Home</li></Link>
+                            <img className='w-5' src={assets.home} alt="" />
+                        </div>
+                        <div className='flex gap-5'>
+                            <Link to="/" ><li>Home</li></Link>
+                            <img className='w-5' src={assets.home} alt="" />
+                        </div>
+                        <div className='flex gap-5'>
+                            <Link to="/" ><li>Home</li></Link>
+                            <img className='w-5' src={assets.home} alt="" />
+                        </div>
+                        <div className='flex gap-5'>
+                            <Link to="/" ><li>Home</li></Link>
+                            <img className='w-5' src={assets.home} alt="" />
+                        </div>
                     </ul>
                 </div>
             </motion.div>
