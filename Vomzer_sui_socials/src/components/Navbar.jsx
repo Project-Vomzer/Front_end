e55146a8f4557d6fa3238005feb0f50d1fb341ea
davidojo1144 <davco1144@gmail.com>
2025-05-14 20:35:23 +0100

    Vomzer Sui

--- Vomzer_sui_socials/src/components/Navbar.jsx
+++ Vomzer_sui_socials/src/components/Navbar.jsx
@@ -13,14 +13,38 @@ const Navbar = () => {
         <div className='md:flex items-center justify-evenly hidden pt-10'>
             <img onClick={()=> navigate("/")} className='w-10 cursor-pointer' src={assets.Vomzer} alt="" />
             <input className='rounded-2xl px-8 py-1 text-black outline-none ' type="text" placeholder='Search....'/>
-            <img onClick={()=> navigate("/")} className='w-7 cursor-pointer' src={assets.home} alt="" />
-            <img className='w-7 cursor-pointer' src={assets.feeds} alt="" />
-            <img className='w-7 cursor-pointer' src={assets.notification} alt="" />
-            <img className='w-7 cursor-pointer' src={assets.chatmessages} alt="" />
-            <img className='w-7 cursor-pointer' src={assets.wallet} alt="" />
-            <img className='w-7 cursor-pointer' src={assets.services} alt="" />
-            <img className='w-7 cursor-pointer' src={assets.Dots} alt="" />
-            <img onClick={()=> navigate("/login")} className='w-7 cursor-pointer' src={assets.login} alt="" />
+            <div className='block items-center '>
+              <img onClick={()=> navigate("/")} className='w-7 cursor-pointer' src={assets.home} alt="" />
+              <p className='text-white text-sm'>Home</p>
+            </div>
+            <div className='block items-center '>
+              <img className='w-7 cursor-pointer' src={assets.feeds} alt="" />
+              <p className='text-white text-sm'>Feeds</p>
+            </div>
+            <div className='block items-center justify-center'>
+              <img className='w-7 cursor-pointer' src={assets.notification} alt="" />
+              <p className='text-white text-sm '>Notifications</p>
+            </div>
+            <div className='block items-center justify-center'>
+              <img className='w-7 cursor-pointer' src={assets.chatmessages} alt="" />
+              <p className='text-white text-sm '>Messages</p>
+            </div>
+            <div className='block items-center justify-center'>
+              <img className='w-7 cursor-pointer' src={assets.wallet} alt="" />
+              <p className='text-white text-sm '>Wallet</p>
+            </div>
+            <div className='block items-center justify-center'>
+              <img className='w-7 cursor-pointer' src={assets.services} alt="" />
+              <p className='text-white text-sm '>Services</p>
+            </div>
+            <div className='block items-center justify-center'>
+              <img className='w-7 cursor-pointer' src={assets.Dots} alt="" />
+              <p className='text-white text-sm '>Menu</p>
+            </div>
+            <div className='block items-center justify-center'>
+              <img onClick={()=> navigate("/login")}  className='w-7 cursor-pointer' src={assets.login} alt="" />
+              <p className='text-white text-sm '>Login</p>
+            </div>
         </div>
         <ResponsiveNavbar/>
     </div>
